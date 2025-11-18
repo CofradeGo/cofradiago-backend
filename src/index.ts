d04c154b660a42f0aeffb7d1fe64cd3fc9892d71
Ángel Cárdenas <angelcardenasrod@gmail.com>
2025-11-18 17:29:15 +0100

    feature/HU-BE-06 Añadir Dockerfile base con Node 22

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use("/health", healthRouter);
 // Solo levantar el servidor si no estamos en test
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
