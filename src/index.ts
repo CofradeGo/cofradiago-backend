ffe0d90404fb28846712b332ce828402103b4cad
Ángel Cárdenas <angelcardenasrod@gmail.com>
2025-12-28 16:56:25 +0100

    feature/HU-BE-CF-03 — Crear cortejos de una cofradía

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import userRoutes from "./routes/user.routes.ts";
 import passwordRoutes from "./routes/password.routes.ts";
 import hermanoRouter from "./routes/hermano.route.ts";
 import cofradiaRouter from "./routes/cofradia.route.ts";
+import cortejosRouter from "./routes/cortejo.route.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -41,6 +42,7 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/password", passwordRoutes);
 app.use("/api/v1/hermanos", hermanoRouter);
 app.use("/api/v1/cofradia", cofradiaRouter);
+app.use("/api/v1/cofradias/:cofradiaId/cortejos", cortejosRouter);
 
 // Archivos estáticos
 app.use("/uploads/logos", express.static(path.join(__dirname, "..", "uploads/logos")));
